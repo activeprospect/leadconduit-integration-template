d77ac1936bf3f961e1ad4ba3caca8dc5241cfdf9
Chris Grayson <chris@activeprospect.com>
2019-02-25 11:07:24 -0600

    switch to use nock-nock

--- test/handle_spec.js
+++ test/handle_spec.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const nock = require('nock');
+const nocknock = require('nock-nock');
 const integration = require('../lib/outbound/handle');
 const parser = require('leadconduit-integration').test.types.parser(integration.requestVariables());
 
@@ -24,14 +24,19 @@ describe('Handle', () => {
 
   describe('Handle', () => {
 
-    afterEach(() => {
-      nock.cleanAll();
+    let recorder = null;
+    afterEach((done) => {
+      if(recorder) {
+        recorder.after(done);
+      }
+      else {
+        done();
+      }
     });
 
     it('should succeed with good input', (done) => {
-      nock('https://example.com')
-        .post('/lookup/123')
-        .reply(200, '{ "number": 42 }', {'Content-Type': 'application/json'});
+      recorder = nocknock('successful_post');
+      recorder.before();
 
       integration.handle(parser({lead: {email: 'foo@bar.com'}}), (err, event) => {
         assert.equal(event.handle.outcome, 'success');
