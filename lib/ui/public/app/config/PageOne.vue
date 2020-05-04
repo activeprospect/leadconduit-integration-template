79bc08a0f3c0f73b555d01cd023a3e88f3b15433
jonathan grayson <jonathan@activeprospect.com>
2020-05-04 10:20:36 -0500

    wip vue template

---
+++ lib/ui/public/app/config/PageOne.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navigation
+      :show-next="listId"
+      >
+      <form>
+        <h5>Enter a made-up List ID:</h5>
+        <input
+            v-model="listId"
+            type="text"
+        >
+      </form>
+    </Navigation>
+  </div>
+</template>
+<script>
+  import Navigation from "./components/Navigation";
+  export default {
+    data() {
+      return {
+        listId: '',
+      };
+    },
+    components: {Navigation}
+  }
+</script>
---
+++ lib/ui/public/app/config/PageTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navigation
+
+    >
+    <section>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Result of API call to S3: </p>
+
+      <p>Result of API call to get lists:</p>
+    </section>
+  </Navigation>
+</template>
+<script>
+  import Navigation from "./components/Navigation";
+  export default {
+    components: {Navigation}
+  }
+</script>
