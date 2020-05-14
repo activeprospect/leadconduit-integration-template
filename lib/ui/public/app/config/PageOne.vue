d6997b706bd2f11e4045dcf0cfb5ae712f9f1eef
jgrayson676 <jonathan@activeprospect.com>
2020-05-14 09:16:08 -0500

    Vue template (#1)
    * wip vue template
    
    * wip draft
    
    * update dependencies
    
    * code review fixes
    
    * update routing
    
    * update dependency
    
    * fix package.json
    
    * update webpack
    
    * fix page titles

---
+++ lib/ui/public/app/config/PageOne.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Navigation
+      title="Set up Service_Being_Integrated"
+      :disabled="!apiKey"
+      >
+      <form>
+        <h5>Enter a made-up API key:</h5>
+        <input
+            v-model="apiKey"
+            type="text"
+        >
+      </form>
+    </Navigation>
+  </div>
+</template>
+<script>
+  import Navigation from "./components/Navigation.vue";
+  export default {
+    data() {
+      return {
+        apiKey: '',
+      };
+    },
+    components: {Navigation}
+  }
+</script>
---
+++ lib/ui/public/app/config/PageTwo.vue
@@ -0,0 +1,51 @@
+<template>
+  <Navigation
+    title="Set up Service_Being_Integrated"
+    :showNext="false"
+    >
+    <section>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Result of API call to S3: {{ simpleResult }}</p>
+
+      <form>
+        <p>When Service_Being_Integrated returns one of the following, stop processing the lead and mark the lead a Failure:</p>
+        <ul>
+          <li>
+            <label><input type="checkbox" v-model="options.first"> First Problem - Something bad</label>
+          </li>
+          <li>
+            <label><input type="checkbox" v-model="options.second"> Second Problem - Something else bad</label>
+          </li>
+        </ul>
+      </form>
+    </section>
+    <template v-slot:footer>
+      <button @click="finish" class="primary">Finish</button>
+    </template>
+  </Navigation>
+</template>
+<script>
+  import Navigation from "./components/Navigation.vue";
+  export default {
+    data() {
+      return {
+        simpleResult: '',
+        options: {
+          first: false,
+          second: false
+        }
+      }
+    },
+    computed: {
+      simpleResult () {
+        return this.$store.state.simpleResult;
+      }
+    },
+    methods: {
+      finish() {
+        this.$store.dispatch('finish');
+      }
+    },
+    components: {Navigation}
+  }
+</script>
