51f98b223c9682f0f6201ef94ae45501cf0d8ac4
Jarrod Miller <jarrod.miller@activeprospect.com>
2020-07-29 16:11:09 -0500

    Vue fixes (#3)
    * Added support for Eslint Standard Rules + Vue support
    
    * Installed some missing packages and added a comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,21 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2,
-            {"SwitchCase": 1}
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "brace-style": [
-            "error",
-            "stroustrup",
-            {"allowSingleLine": true}
-        ],
-        "quotes": [
-            "error",
-            "single",
-            {"allowTemplateLiterals": true}
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    },
-    "overrides": [
-        {
-            "files": ["test/fixtures/**"],
-            "rules": {
-                "quotes": "off",
-                "indent":"off"
-            }
-        }
-    ]
-};
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
