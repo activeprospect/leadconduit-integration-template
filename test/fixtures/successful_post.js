d29eed60f075f5838ae285740340c23f573338e0
Chris Grayson <chris@activeprospect.com>
2021-10-18 10:18:08 -0500

    minor cleanup & lint fixes (#9)

--- test/fixtures/successful_post.js
+++ test/fixtures/successful_post.js
@@ -1,18 +1,18 @@
-var nock = require('nock');
+const nock = require('nock');
 
-nock('https://example.com:443', {"encodedQueryParams":true})
-  .post('/lookup/123', "email=foo@bar.com")
-  .reply(200, {"outcome":"success","number":42}, [ 'Cache-Control',
-  'max-age=604800',
-  'Content-Type',
-  'text/html; charset=UTF-8',
-  'Date',
-  'Mon, 25 Feb 2019 17:04:04 GMT',
-  'Expires',
-  'Mon, 04 Mar 2019 17:04:04 GMT',
-  'Server',
-  'EOS (vny006/0451)',
-  'Content-Length',
-  '61',
-  'Connection',
-  'close' ]);
+nock('https://example.com:443', { encodedQueryParams: true })
+  .post('/lookup/123', 'email=foo@bar.com')
+  .reply(200, { outcome: 'success', number: 42 }, ['Cache-Control',
+    'max-age=604800',
+    'Content-Type',
+    'text/html; charset=UTF-8',
+    'Date',
+    'Mon, 25 Feb 2019 17:04:04 GMT',
+    'Expires',
+    'Mon, 04 Mar 2019 17:04:04 GMT',
+    'Server',
+    'EOS (vny006/0451)',
+    'Content-Length',
+    '61',
+    'Connection',
+    'close']);
