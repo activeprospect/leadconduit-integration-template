331c65e44c10bee41d4f1ef728751d6e61abc2d9
Chris Grayson <chris@activeprospect.com>
2024-10-11 16:22:30 -0500

    change Page2 to idiomatic 'stop or continue on failure?' config

--- lib/ui/public/app/config/Page2.vue
+++ lib/ui/public/app/config/Page2.vue
@@ -4,17 +4,15 @@
       Set up Service_Being_Integrated
     </header>
     <section>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <p>Result of API call to S3: {{ simpleResult }}</p>
-
       <form>
-        <p>When Service_Being_Integrated returns one of the following, stop processing the lead and mark the lead a Failure:</p>
+        <p>When the response is a Failure:</p>
         <ul>
           <li>
-            <label><input type="checkbox" v-model="options.first"> First Problem - Something bad</label>
+            <label><input type="radio" v-model="failureAction" value="stop">Stop the lead flow and mark the lead a Failure
+              (Recommended)</label>
           </li>
           <li>
-            <label><input type="checkbox" v-model="options.second"> Second Problem - Something else bad</label>
+            <label><input type="radio" v-model="failureAction" value="continue">Continue the lead flow</label>
           </li>
         </ul>
       </form>
@@ -27,15 +25,12 @@ import { Navigation } from '@activeprospect/integration-components';
 export default {
   data () {
     return {
-      simpleResult: '',
-      options: {
-        first: false,
-        second: true
-      }
+      failureAction: this.$store.getters.getFailureAction
     };
   },
   methods: {
     finish () {
+      this.$store.commit('setFailureAction', this.failureAction);
       this.$store.dispatch('finish');
     }
   },
